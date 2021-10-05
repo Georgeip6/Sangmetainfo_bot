53ad801d0bca4d9b69f6e4407fb8a3327833949f
Georgeip6 <88154012+Georgeip6@users.noreply.github.com>
2021-10-05 15:08:02 +0300

    Update SangMetainfo_bot.py

--- SangMetainfo_bot.py
+++ SangMetainfo_bot.py
@@ -10,7 +10,7 @@ ON_HEROKU = os.environ.get('ON_HEROKU')
 
 if ON_HEROKU:
     # get the heroku port
-    port = int(os.environ.get('PORT', 17995))  # as per OP comments default is 17995
+    port = int(os.environ.get('PORT','17995'))  # as per OP comments default is 17995
 else:
     port = 3000
 TOKEN = os.environ.get('BOT_TOKEN',None)
